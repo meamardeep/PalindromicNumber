6916c0441745ef3e73b2701d25585584bc478132
Saurabh Kumar <usersaurabh92@gmail.com>
2020-07-29 18:59:12 +0530

    Initial commit

---
+++ PalindromicNumber/PalindromicNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PalindromicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number;
+            Console.WriteLine("Enter a nnumber to check whether its palindome or not");
+            number = long.Parse(Console.ReadLine());
+            long reverseNumber = 0;
+
+            while (number != 0)
+            {
+                reverseNumber = (reverseNumber * 10) + (number % 10);
+                number = number / 10;
+            }
+
+            if (reverseNumber == number)
+            {
+                Console.WriteLine("Entered Number is palindrome");
+            }
+            else
+                Console.WriteLine("Entered number is not palindrome");
+
+            Console.ReadLine();
+        }
+    }
+}
